8159b05f43358705271d1e8f8af00fdd23d7daeb
Facundo Garbino <facundo@hyros.com>
2022-10-15 20:29:33 -0300

    Add new script

--- raspi/script.py
+++ raspi/script.py
@@ -1,15 +1,21 @@
 import time
 import cv2
-import matplotlib.pyplot as plt
 
 def check_image(image):
+    cv2.imwrite('imagen.png', image)
 
-
-
-    
+#Initialize camera
 cam = cv2.VideoCapture(0)
 
+for i in range(30):
+  temp = cam.read()
+
+ret, image = cam.read()
+
+
 while (True):
-    result, image = cam.read()
+  time.sleep(1)
+  ret, image = cam.read()
+
+  if ret:
     check_image(image)
-    sleep(1)
